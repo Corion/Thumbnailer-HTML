74ca95495a259c1cfd0030b84c7f8f5c78caaded
Max Maischein <corion@corion.net>
2020-02-16 13:50:09 +0100

    Render the page width at 256 pixels instead of cropping it there
    ... at least for responsive pages, this will work out well

--- wkhtmltothumbnail.sh
+++ wkhtmltothumbnail.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # We set a fake, empty proxy here to prevent wkhtmltoimage from talking to the outside world :-((
-exec /usr/bin/wkhtmltoimage --proxy http://127.0.0.2:6667 --disable-javascript --disable-plugins --allow . -f png --crop-w 256 --crop-h 256 "$1" "$2" >/dev/null 2>/dev/null
+exec /usr/bin/wkhtmltoimage --proxy http://127.0.0.2:6667 --disable-javascript --disable-plugins --allow . -f png --width 256 --crop-h 256 "$1" "$2" >/dev/null 2>/dev/null
